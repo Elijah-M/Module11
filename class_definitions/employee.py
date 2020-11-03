0a21cca2fe8ed0e06d0ad8c91ef389fcca76c2cc
Elijah Morishita <elijah.lm@gmail.com>
2020-11-02 22:26:35 -0600

    6th Commit - Employee class with hourly & salary derived classes

--- class_definitions/employee.py
+++ class_definitions/employee.py
@@ -8,50 +8,82 @@ This program is used for creating an example of multiple derived classes
 
 class Employee:
 
-    # The constructor
-    def __init__(self, last_name, first_name, address, phone_number, salaried, start_date, salary):
-        """
-        This constructor sets the variables
-        :param last_name:
-        :param first_name:
-        :param address:
-        :param phone_number:
-        :param salaried:
-        :param start_date:
-        :param salary:
-        """
+    def __init__(self, last_name="Morishita", first_name="Elijah", address="123 Main St.\nDes Moines, IA 50265",
+                 phone_number="(555) 555-5555"):
+
         self._last_name = last_name
         self._first_name = first_name
         self._address = address
         self._phone_number = phone_number
-        self._salaried = salaried
+
+    def display(self):
+
+        return "Name: " + self._last_name + ', ' + self._first_name + '\n'\
+        + "Address: " + self._address + '\n'\
+        + "Phone Number: " + self._phone_number
+
+
+class SalariedEmployee(Employee):
+    """The SalariedEmployee class which derives from Employee"""
+    def __init__(self, start_date, salary=40000):
+        super().__init__()
         self._start_date = start_date
         self._salary = salary
 
-    def display(self):
-        """
-        This function brings the variables together and formats them based
-        on if the contructors parameters are either Salaried or Hourly
-        :return: either a salried or Hourly string
-        """
-        if self._salaried == "Salaried":
-            info = str(self._first_name + " " + self._last_name + "\n" +
-                       self._address + "\n" +
-                       self._phone_number + "\n" +
-                       "Salaried Employee: " + self._salary + "/year" + "\n" +
-                       "Start Date: " + self._start_date)
-            return info
+    def give_raise(self, salary):
+        self._salary = salary
 
-        elif self._salaried == "Hourly":
-            info = str(self._first_name + " " + self._last_name + "\n" +
-                       self._address + "\n" +
-                       self._phone_number + "\n" +
-                       "Hourly Employee: " + self._salary + "/hour" + "\n" +
-                       "Start Date: " + self._start_date)
-            return info
+    def display(self):
+        salary = str(self._salary)
+        return "Name: " + self._last_name + ', ' + self._first_name + '\n'\
+        + "Start Date: " + self._start_date + '\n'\
+        + "Salary: $" + salary
+
+
+class HourlyEmployee(Employee):
+    """The HourlyEmployee class which derives from Employee"""
+    def __init__(self, start_date, hourly_pay=10.00):
+        super().__init__()
+        self._start_date = start_date
+        self._hourly_pay = hourly_pay
+
+    def give_raise(self, hourly_pay):
+        self._hourly_pay = hourly_pay
+
+    def display(self):
+        hourly_pay = str(self._hourly_pay)
+        return "Name: " + self._last_name + ', ' + self._first_name + '\n'\
+        + "Start Date: " + self._start_date + '\n'\
+        + "Hourly Pay: $" + hourly_pay
 
 
 # The Driver
-emp = Employee("Morishita", "Elijah", "123 Main St.\nDes Moines, IA 50265", "(555) 555-5555", "Salaried", "10/26/2001", "100,000.00")
-print(emp.display())
-del emp
+supervisor = SalariedEmployee("11/2/2020")
+print(supervisor.display())  # printing the SalariedEmployee object
+divider = '=' * 40  # just a divider
+print(divider)  # ===================
+CEO = Employee()
+print(CEO.display())  # Displaying the Employee object
+print(divider)  # ===================
+supervisor.give_raise(45000)
+print(supervisor.display())  # printing the SalariedEmployee object
+print(divider)  # ===================
+CEO = Employee()
+print(CEO.display())  # Displaying the Employee object again
+print(divider)  # ===================
+assembler = HourlyEmployee("11/2/2020")
+print(assembler.display())  # printing the HourlyEmployee object
+print(divider)  # ===================
+CEO = Employee()
+print(CEO.display())  # Displaying the Employee object again
+print(divider)  # ===================
+assembler.give_raise(12.00)
+print(assembler.display())  # printing the HourlyEmployee object
+print(divider)  # ===================
+CEO = Employee()
+print(CEO.display())  # Displaying the Employee object again
+
+# Garbage collection
+del supervisor
+del assembler
+del CEO
