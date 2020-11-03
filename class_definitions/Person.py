4f16daa38e4ec3f3c7109e4837e0e9a85ba083ca
Elijah Morishita <elijah.lm@gmail.com>
2020-11-02 20:31:09 -0600

    4th Commit - class Student(Person)

--- class_definitions/Person.py
+++ class_definitions/Person.py
@@ -1,12 +1,42 @@
+"""
+Author: Elijah Morishita
+elmorishita@dmacc.edu
+11/2/2020
+This program uses a Student class that derives from the Person class
+and prints out student info to the screen
+"""
+
+
 class Person:
-    """Person class using class Address, Class Composition"""
+    """Person class"""
     def __init__(self, lname, fname, addy=''):
-        name_characters = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'-")
-        if not (name_characters.issuperset(lname) and name_characters.issuperset(fname)):
-            raise ValueError
-        self.last_name = lname
-        self.first_name = fname
-        self.address = addy
+        self._last_name = lname
+        self._first_name = fname
+        self._address = addy
 
     def display(self):
-        return str(self.last_name) + ", " + str(self.first_name) + '\n' + self.address.display()
+        return self._last_name + ", " + self._first_name + ":" + self._address
+
+
+class Student(Person):
+    """ The Student Class, derived from Person"""
+    def __init__(self, student_id, lname='UNK', fname='UNK', major="Computer Science", gpa='0.0'):
+        super().__init__(lname, fname)
+        self._student_id = student_id
+        self._major = major
+        self._gpa = gpa
+
+    def display(self):
+        stu_id_str = str(self._student_id)
+        gpa_str = str(self._gpa)
+        return self._last_name + ', ' + self._first_name + ':(' + stu_id_str + ') ' + self._major + ' gpa: ' + gpa_str
+
+
+# Driver
+my_student = Student(900111111, 'Song', 'River')
+print(my_student.display())
+my_student = Student(900111111, 'Song', 'River', 'Computer Engineering')
+print(my_student.display())
+my_student = Student(900111111, 'Song', 'River', 'Computer Engineering', 4.0)
+print(my_student.display())
+del my_student
